6d372fbcea9cb3c2511ea8a075f8bdd1a3e5c0ae
Andreas Sundqvist <andreas@stefna.is>
2025-07-07 11:08:46 +0200

    Change datadog processor to improve exception logging
    Use datadog recommended log format for errors this makes the errors
    render much nice in datadog ui

--- src/Processor/DatadogProcessor.php
+++ src/Processor/DatadogProcessor.php
@@ -11,34 +11,13 @@ final class DatadogProcessor implements ProcessorInterface
 	public function __invoke(LogRecord $record): LogRecord
 	{
 		$context = $record->context;
-		$tags = [];
-		if (isset($context[DatadogHandler::TAGS]) && !is_array($context[DatadogHandler::TAGS])) {
-			foreach ($context[DatadogHandler::TAGS] as $key => $value) {
-				$tags[] = (is_string($key) ? $key . ':' : '') . $value;
-			}
-			// remove from context to avoid duplicate data
-			unset($context[DatadogHandler::TAGS]);
+		if (isset($context['exception']) && $context['exception'] instanceof \Throwable) {
+			$context['error.message'] = $context['exception']->getMessage();
+			$context['error.stack'] = $context['exception']->getTraceAsString();
+			$context['error.kind'] = $context['exception']::class;
+			unset($context['exception']);
 		}
 
-		if ($record->channel) {
-			$tags[] = 'channel:' . $record->channel;
-		}
-
-		// todo needs to be moved from processor to handler since we can't modify the record
-		/*
-		// needs to be at top level for datadog to handle them
-		$record[DatadogHandler::TAGS] = implode(',', $tags);
-
-		if (isset($context[DatadogHandler::SERVICE]) && !isset($record[DatadogHandler::SERVICE])) {
-			$record[DatadogHandler::SERVICE] = $context[DatadogHandler::SERVICE];
-			unset($context[DatadogHandler::SERVICE]);
-		}
-
-		if (isset($context[DatadogHandler::HOSTNAME]) && !isset($record[DatadogHandler::HOSTNAME])) {
-			$record[DatadogHandler::HOSTNAME] = $context[DatadogHandler::HOSTNAME];
-			unset($context[DatadogHandler::HOSTNAME]);
-		}
-		*/
-		return $record;
+		return $record->with(context: $context);
 	}
 }
